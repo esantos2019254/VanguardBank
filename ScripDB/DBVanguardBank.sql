a7c389612ce70ec99b162ee7d55f5ebc47e154ea
Elmer Santos <esantos-2019254@kinal.edu.gt>
2023-07-07 15:12:18 -0600

    Se agrego la estructura de la DB

--- ScripDB/DBVanguardBank.sql
+++ ScripDB/DBVanguardBank.sql
@@ -4,4 +4,43 @@ Create database DBVanguardBank;
 
 Use DBVanguardBank;
 
--- Entidad --
+-- Entidad Clientes--
+
+
+-- Entidad Departamentos --
+
+
+-- Entidad Proveedores --
+
+
+-- Entidad Tipo Moneda --
+
+
+-- Entidad Tipo Empleado --
+
+
+-- Entidad Empleados --
+
+
+-- Entidad Sucursales --
+
+
+-- Entidad Cuentas --
+
+
+-- Entidad Créditos --
+
+
+-- Entidad Servicios --
+
+
+-- Entidad Login --
+
+
+-- Entidad Transacciones --
+
+
+-- Entidad Historial Transacción --
+
+
+-- Entidad Depositos --
